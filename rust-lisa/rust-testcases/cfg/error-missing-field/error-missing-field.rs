cfddfb8e2f7a26e67a4c0b8d39eb5e9aaed59f96
Simone Gazza <gazza.sim@gmail.com>
2022-10-17 20:20:33 +0200

    #4 Add error-test for missing field

---
+++ rust-lisa/rust-testcases/cfg/error-missing-field/error-missing-field.rs
@@ -0,0 +1,11 @@
+enum Message {
+    Move { x: i32, y: i32 },
+}
+
+fn main() {
+    let m = Message::Move {x : 1, y : 2};
+    if let Message::Quit = m2 {
+        println!("{}", a);
+        println!("{}", b);
+    }
+}
--- rust-lisa/src/test/java/it/unipr/frontend/cfg/RustCFGFieldExistance.java
+++ rust-lisa/src/test/java/it/unipr/frontend/cfg/RustCFGFieldExistance.java
@@ -13,4 +13,17 @@ public class RustCFGFieldExistance extends RustLiSATestExecutor {
 		perform("cfg/field-existance", "field-existance.rs", conf);
 	}
 
+	@Test
+	public void testErrorMissingField() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration().setDumpCFGs(true).setJsonOutput(true);
+		try {
+			perform("cfg/error-missing-field", "error-missing-field.rs", conf);
+		} catch (UnsupportedOperationException e) {
+			// We want to catch exactly this type of error, all good here
+			if (!(e.getMessage().contains("Use of undeclared struct field in"))) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
+
 }
