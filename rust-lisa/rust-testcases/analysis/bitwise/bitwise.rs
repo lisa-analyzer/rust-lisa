baf700eee39393082c716949e0228d2b5d3a52ed
Simone Gazza <gazza.sim@gmail.com>
2022-10-26 17:37:25 +0200

    #25 Add test analysis for previous implemented semantics

---
+++ rust-lisa/rust-testcases/analysis/bitwise/bitwise.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = 2;
+    let y = x << 2;
+    let z = y >> 2;
+    let _q = x & y;
+    let v = x | z;
+    let _w = 4096 ^ v;
+}
---
+++ rust-lisa/rust-testcases/analysis/let/let.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x = 42;
+    let y = "Hello world";
+    let z = 4.7;
+}
