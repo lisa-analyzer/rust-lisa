f8ec03649b47b9b2965a3b099274762c6f0520a4
Simone Gazza <gazza.sim@gmail.com>
2022-10-28 11:45:41 +0200

    #25 Add short circuit semantics test

---
+++ rust-lisa/rust-testcases/analysis/logical/logical.rs
@@ -0,0 +1,23 @@
+fn main() {
+	let x = true;
+	let y = false;
+	
+	let z = y && x;
+	let w = x && y;
+	
+	let q = x || y;
+	let p = y || x;
+	
+	let r = !x;
+	let s = !y;
+	
+	let a = 1;
+	let b = 2;
+	
+	let c = a > b;
+	let d = a < b;
+	let e = a <= a;
+	let f = a > b;
+	let g = a >= b;
+
+}
