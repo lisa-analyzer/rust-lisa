5ab4e829142dcdf0edece7057580f0162aa5ebfc
Simone Gazza <gazza.sim@gmail.com>
2022-11-08 15:44:38 +0100

    #20 Test for Trait and struct integration

--- rust-lisa/rust-testcases/cfg/trait/trait.rs
+++ rust-lisa/rust-testcases/cfg/trait/trait.rs
@@ -2,7 +2,7 @@ trait Measure {
     fn area(&self) -> f32;
     fn perimeter(&self) -> f32;
 }
-/*
+
 struct Circle {
     x: f32,
     y: f32,
@@ -15,7 +15,6 @@ impl Circle {
     }
 }
 
-
 impl Measure for Circle {
     fn area(&self) -> f32 {
         self.r * self.r * 3.14
@@ -25,7 +24,7 @@ impl Measure for Circle {
         2.0 * self.r * 3.14
     }
 }
-*/
+
 fn main() {
     let circle = Circle { x : 0.0, y : 0.0, r : 1.0 };
     
